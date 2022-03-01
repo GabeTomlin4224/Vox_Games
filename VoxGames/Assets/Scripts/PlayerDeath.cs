7dc24d49917a6a48929477c70f7b8c4ca0beb147
austin <73039393+Atvmix@users.noreply.github.com>
2022-03-01 09:51:10 -0500

    NGT

--- VoxGames/Assets/Scripts/PlayerDeath.cs
+++ VoxGames/Assets/Scripts/PlayerDeath.cs
@@ -1,22 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            Destroy(gameObject);
-            LevelManager.instance.Respawn();
-        }
+    
+    [SerializeField] Vector2 deathKick = new Vector2(40f, 40f);
+    [SerializeField] int playerLives = 3;
+    bool isAlive = true;
 
-        if (collision.gameObject.CompareTag("Hazard"))
+    Rigidbody2D myRigidBody;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
+    
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if(!isAlive)
         {
-            Destroy(gameObject);
-            LevelManager.instance.Respawn();
+            return;
+        }
+        Die();
+    }
+
+
+    private void Die()
+    {
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
+        {
+            isAlive = false;
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            FindObjectOfType<PlayerDeath>().ProcessDeath();
         }
     }
 
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+
+
+    private void TakeLife()
+    {
+        playerLives--;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+
+    private void ProcessDeath()
+    {
+        if (playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            Restart();
+        }
+    }
 }
