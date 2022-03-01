d9eee18ca6b721322aa3b01605740633c7d2809b
NoahB0 <90911726+NoahB0@users.noreply.github.com>
2022-03-01 09:05:37 -0500

    Revert "NGT"
    This reverts commit 4fe1c3bdd7c5917873a314e72028f58103bbb4d8, reversing
    changes made to 975ed13d92bf3257e75340259e7c20ccca46e284.

--- VoxGames/Assets/Scripts/PlayerDeath.cs
+++ VoxGames/Assets/Scripts/PlayerDeath.cs
@@ -4,35 +4,19 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-   [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
-  
-  
-   bool isAlive = true;
-   CapsuleCollider2D myBodyCollider;
-   Rigidbody2D myRigidBody;
-   void Start ()
-   {
-       myBodyCollider = GetComponent<CapsuleCollider2D>();
-   }
-   
-   
-   void Update ()
-   {
-       if (!isAlive)
-       {
-           return;
-       }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            LevelManager.instance.Respawn();
+        }
 
-       Die();
-   }
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            Destroy(gameObject);
+            LevelManager.instance.Respawn();
+        }
+    }
 
-
-   private void Die()
-   {
-       if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
-       {
-           isAlive = false;
-           GetComponent<Rigidbody2D>().velocity = deathKick;
-       }
-   }
 }
