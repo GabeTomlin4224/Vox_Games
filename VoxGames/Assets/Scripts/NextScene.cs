2f402c80a5aafb5f63fae239c1a1238087231e07
NoahB0 <90911726+NoahB0@users.noreply.github.com>
2022-03-02 09:07:36 -0500

    NGT

---
+++ VoxGames/Assets/Scripts/NextScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+     public void StartFirstLevel()
+     {
+         SceneManager.LoadScene(2);
+     }
+
+}
+
+
+     
+  
+    
+    
